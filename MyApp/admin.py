a25b03b8444e852f85f405f99419da03ebbcbd89
Connor Gregorius <gregor66@uwm.edu>
2026-02-12 13:24:02 -0600

    Database setup

--- MyApp/admin.py
+++ MyApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Role, Profile
 
-# Register your models here.
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user",)
+    search_fields = ("user__username", "user__email")
+    filter_horizontal = ("roles",)
--- MyApp/models.py
+++ MyApp/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Role(models.Model):
+    # Name will be like unhoused, donor, and volunteer
+    # If we want to combine donor and volunteer thats fine, we can probably then just
+    # add it into the profile model and remove this model entirely
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    roles = models.ManyToManyField(Role, blank=True)
+
+    def __str__(self):
+        return f"Profile: {self.user.username}"
+
