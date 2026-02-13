0b2ce78f79324d71f84b22ef61e5ff7a2318168d
JHuang95 <huang95@uwm.edu>
2026-02-13 06:03:55 -0600

    Initial resource route, view, template

--- MyApp/urls.py
+++ MyApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, resources
 
 urlpatterns = [
     path("", home, name="home.html"),
+    path("resources/", resources, name = "resources.html")
 ]
