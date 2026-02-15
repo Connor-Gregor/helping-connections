840d67b05e6801cb6d3605d6ab41907d1bd20d36
Connor Gregorius <gregor66@uwm.edu>
2026-02-15 14:39:45 -0600

    Account creation and login

--- MyApp/urls.py
+++ MyApp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home, resources
+from .views import home, resources, Register, LoginView, logout_view
 
 urlpatterns = [
-    path("", home, name="home.html"),
-    path("resources/", resources, name = "resources.html")
+    path("", home, name="home"),
+    path("resources/", resources, name="resources"),
+    path("register/", Register.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", logout_view, name="logout"),
 ]
