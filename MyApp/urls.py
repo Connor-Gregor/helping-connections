4007fe8faff8c196c607f374704503a6d56f194b
Lilbang414 <kareemsalem02@gmail.com>
2026-02-19 19:17:42 -0600

    Dashboards for both user types

--- MyApp/urls.py
+++ MyApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, resources, Register, LoginView, logout_view, settings_page, update_profile_settings, change_password, find_help
+from .views import home, resources, Register, LoginView, logout_view, settings_page, update_profile_settings, change_password, find_help, volunteer, unhoused
 
 urlpatterns = [
     path("", home, name="home"),
@@ -11,4 +11,6 @@ urlpatterns = [
     path("settings/profile/", update_profile_settings, name="settings_profile"),
     path("settings/password/", change_password, name="settings_password"),
     path("find-help/", find_help, name="find_help"),
+    path('volunteer/', volunteer, name='volunteer'),
+    path('unhoused_dash/', unhoused, name='unhoused'),
 ]
