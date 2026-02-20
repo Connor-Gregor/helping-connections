e101fabc396719a5da0d5340f00f60a8300ec473
Connor Gregorius <gregor66@uwm.edu>
2026-02-20 08:28:40 -0600

    fixed page routing

--- MyApp/urls.py
+++ MyApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import home, resources, Register, LoginView, logout_view, settings_page, update_profile_settings, change_password, find_help, volunteer, unhoused, account_view
+from .views import home, resources, Register, LoginView, logout_view, settings_page, update_profile_settings, \
+    change_password, find_help, volunteer, unhoused, account_view, dashboard_redirect
 
 urlpatterns = [
     path("", home, name="home"),
@@ -17,4 +18,5 @@ urlpatterns = [
     path("find-help/", find_help, name="find_help"),
     path('volunteer/', volunteer, name='volunteer'),
     path('unhoused_dash/', unhoused, name='unhoused'),
+    path("dashboard/", dashboard_redirect, name="dashboard_redirect")
 ]
